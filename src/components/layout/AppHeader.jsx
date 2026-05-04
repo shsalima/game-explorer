78a0c19c341e4283ccc649597268953d026f5147
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-04 21:30:56 +0100

    Set up the app layout header

---
+++ src/components/layout/AppHeader.jsx
@@ -0,0 +1,11 @@
+export default function AppHeader() {
+    return (
+        <header className="bg-[#010102] text-white py-5">
+            <div className="container flex justify-between items-center">
+                <a href="./" className="text-xl">
+                    GameExplorer
+                </a>
+            </div>
+        </header>
+    );
+}
