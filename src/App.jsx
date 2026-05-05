cb6a1db238e71c5c5043d1379cbccf6d22520ca9
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-05 15:28:48 +0100

    Set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppHeader from "./components/layout/AppHeader";
+import { Routes } from "react-router";
 
 function App() {
     return (
         <>
             <AppHeader />
+            <Routes></Routes>
         </>
     );
 }
