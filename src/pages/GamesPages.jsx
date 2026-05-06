b917dd452f1058d4cade9bfd604c548577b3d922
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-06 22:39:33 +0100

    Set up games page

---
+++ src/pages/GamesPages.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../components/global/SectionTitle";
+
+export default function GamesPage() {
+    return (
+        <div className="bg-[#080b10]">
+            <div className="container">
+                <SectionTitle category={"Explore"} title={"All Games"} />
+            </div>
+        </div>
+    );
+}
