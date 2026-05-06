749aff633e1b25b6ed5d7ac049ff5d14453589e9
salima <sahisalima77@gmail.com>
2026-05-06 11:29:42 +0200

    déclare page creator au layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import AppHeader from "./components/layout/AppHeader";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import CreatorsPage from "./pages/CreatorsPage";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <AppHeader />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/creators" element={<CreatorsPage/>}/>
             </Routes>
         </>
     );
