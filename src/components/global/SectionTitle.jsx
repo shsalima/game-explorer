ea9368e091d4ab93d899c0621e6b141a64704bcc
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-06 17:30:59 +0100

    Set up trending games section and created a global section title component

---
+++ src/components/global/SectionTitle.jsx
@@ -0,0 +1,10 @@
+export default function SectionTitle({ category, title }) {
+    return (
+        <div id="section-title">
+            <span className="text-[#38bdf8] pl-10 relative before:absolute before:bg-[#38bdf8] before:left-0 before:top-[39%] translate-y-[-50%] before:w-[30px] before:h-[4px]">
+                {category}
+            </span>
+            <h2 className="text-white text-3xl font-bold">{title}</h2>
+        </div>
+    );
+}
---
+++ src/components/home/TrendingGames.jsx
@@ -0,0 +1,9 @@
+import SectionTitle from "../global/SectionTitle";
+
+export default function TrendingGames() {
+    return (
+        <section className="py-10">
+            <SectionTitle category={"Hot Right Now"} title={"Trending Games"} />
+        </section>
+    );
+}
